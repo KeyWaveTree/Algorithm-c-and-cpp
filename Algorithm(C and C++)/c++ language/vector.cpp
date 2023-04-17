b36c2516bb09f69cc5e6c7f7f29709fd56ca4ad7
KeyWaveTree <damrok0815@gmail.com>
2023-04-17 21:12:19 +0900

    c++ example vector and MST prime

---
+++ Algorithm(C and C++)/c++ language/vector.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> v; //type 제너레이터 int, float, char, string, bool, class, typedef struct 등등 
+	v.push_back(10); //push 하지만 할때마다 2제곱씩 늘어난다. 
+	v.assign(10, 10); //앞에 개수, 뒤에 초기값, 이차원 백터.assign(size, vector<type>)
+	v.pop_back(); //return void 타입
+	//백터 요소 확인 v[]
+
+	//백터 메소드 
+	/*size() - 벡터의 요소의 개수를 반환한다.
+
+	swap(vector객체) - 두 벡터의 내용을 교환(교체)한다.
+
+	empty() - 벡터가 비었는지 여부를 반환한다.
+
+	at(index) - index번째 요소에 접근한다.
+
+	front() - 벡터의 첫 번째 요소를 반환한다.
+
+	back() - 벡터의 마지막 요소를 반환한다.
+
+	begin() - 벡터의 첫 번째 요소를 가리킨다.
+
+	end() - 벡터의 마지막 요소를 가리킨다.
+	
+	//이차원 백터 
+	vector<vector<int>> r;
+	r.assign(3, vector<int>());
+	
+	*/
+
+
+}
--- Algorithm(C and C++)/nextop/014_Prim.cpp
+++ Algorithm(C and C++)/nextop/014_Prim.cpp
@@ -1,5 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include<iostream>
+#define N 10
+
+using namespace std;
+
+int arr[N][N];
+int r[N][N]; //출력노드
+bool cheak[N]; //현재 컴퓨터가 간 노드들 체크 why? 무한반복걸리는 사이클 방지 false 아직 안감, true 갔음
+int n=10; // 총 노드 개수
 
 void input()
 {
@@ -8,7 +16,36 @@ void input()
 
 void prim()
 {
+	int i, chk, s, e;
+	int min;
+	int cnt = 0;// 노드 탐색 개수
+	
+	while (true)
+	{
+		min = 1000000000;
+		for (chk = 1; chk <= n; chk++) //시작노드 index
+		{
+			for (i = 1; i <= n; i++) //끝 노드 index
+			{
+				if (arr[chk][i] != 0 && cheak[chk] == false && min > arr[chk][i])
+				{
+					min = arr[chk][i]; //가중치 저장
+					s = chk; //연결되는 처음의 인덱스
+					e = i; //연결되는 끝의 인덱스
+				}
+			}
+		}
 
+		//cheak배열에 다녀온 노드 표기
+		cheak[e] = true;
+
+		// 양방향 위치에다가 최소값 넣기
+		r[s][e] = min; 
+		r[e][s] = min; 
+
+		cnt++; 
+		if (cnt == n - 1) break;
+	}
 }
 
 void print()
