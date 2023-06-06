445a362e3faafc745920a238ddba8fcfcb28525a
KeyWaveTree <damrok0815@gmail.com>
2023-06-06 20:30:55 +0900

    stack car fin

--- Algorithm(C and C++)/baekjoon/e2/자리배정.cpp
+++ Algorithm(C and C++)/baekjoon/e2/자리배정.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-	int n, m;
+	int c, r; //가로, 세로
+	int cc, rr; //가로, 세로 복사 본
+	int site[1000][1000] = { 0 };
+	int cnt = 0;
+	int sw = 1;
 
-	cin >> n >> m;
+	cin >> c >> r;
+	
+	cc = c, rr = r;
 }
